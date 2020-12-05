f9cd0b261644ca9d66f63a02a80377be9e68b165
张嘉威 <zjw153246@sina.com>
2020-12-05 12:05:59 +0800

    第2次提交

--- src/page/mall_main.vue
+++ src/page/mall_main.vue
@@ -1,79 +1,63 @@
 <template>
   <div>
-    <div class="box_head">码帮商城</div>
-    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-      <van-swipe-item>1</van-swipe-item>
-      <van-swipe-item>2</van-swipe-item>
-      <van-swipe-item>3</van-swipe-item>
-      <van-swipe-item>4</van-swipe-item>
-    </van-swipe>
+    <div class="bigg">
+      <div class="box_head">码帮商城</div>
+      <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+        <van-swipe-item class="box_img">
+          <img src="img/banner_1.jpg" />
+        </van-swipe-item>
+        <van-swipe-item class="box_img">
+          <img src="img/banner_2.jpg" />
+        </van-swipe-item>
+      </van-swipe>
+      <div class="block">
+        <div style="background: rgb(242, 242, 242)">
+          <div class="box_1">
+            <div class="goods_img">
+              <a href="#">
+                <img
+                  class="box_commodity"
+                  src="https://img.yzcdn.cn/upload_files/2015/05/13/Fi9mk4S1ve59315BRMx0wXpPgCsA.jpg!middle.jpg"
+                />
+                <div class="img_logo">
+                  <img src="img/logo.png" width="32px" height="32px" />
+                </div>
+              </a>
+            </div>
+            <div class="goods_title">新疆库尔勒香梨，3个仅售9.9元！</div>
+            <div class="box_2" style="width: 135px">￥9.9</div>
+            <div class="iconfont icon-gouwuche box_car"></div>
+          </div>
+          <div class="box_1">
+            <div class="goods_img">
+              <a href="#">
+                <img
+                  class="box_commodity"
+                  src="https://img.yzcdn.cn/upload_files/2016/03/16/FvXCq8Ye4m5XIoCyOI4w7SvwLqqe.jpg!middle.jpg"
+                />
+                <div class="img_logo">
+                  <img src="img/logo.png" width="32px" height="32px" />
+                </div>
+              </a>
+            </div>
+            <div class="goods_title">
+              【商务大切盘，6-8人份！】6种时令水果，企业下午茶、会议茶歇、亲朋聚会，分享快乐，分享精彩！
+            </div>
+            <div class="box_2" style="width: 135px">￥69</div>
+            <div class="iconfont icon-gouwuche box_car"></div>
+          </div>
+
+          <a href="#" class="lookAll">点击查看全部商品</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="plase"></div>
   </div>
 </template>
 
 <script>
 let vueMethods = {};
-// vueMethods.banner = async function () {
-//   let n = 0; //当前显示的图片序号（从0开始计算）
-
-//   let total = 2; //当前图片的总数
-
-//   let eleBoxIn = document.getElementById("id_box_in"); //banner内盒子元素对象
-
-//   //type表示动作类型，next表示下一张，prev表示上一张，否则代表具体的第几涨
-//   let switchimg = function (type) {
-//     if (type == "next") {
-//       //T1：如果type是next，处理下一张的目标位置
-//       n = n + 1;
-//       if (n >= total) {
-//         //如果n大于等于总数
-//         // alert("n超出范围，赋值为0")
-//         n = 0;
-//       }
-//     } else if (type == "prev") {
-//       //T2：如果type是prev，处理上一张的目标位置
-//       n = n - 1;
-//       if (n < 0) {
-//         //如果n小于0
-//         // alert("n超出范围，赋值为2")
-//         n = 2;
-//       }
-//     } else {
-//       //T3:否则
-//       n = type;
-//     }
-
-//     let left = -n * 100 + "%";
-//     // alert(left);
-//     eleBoxIn.style.left = left;
-
-//     let btn_0 = document.getElementById("id_btn_0");
-//     let btn_1 = document.getElementById("id_btn_1");
-//     let bgFocus = "#fff";
-//     let bgNormal = "rgba(255,255,255,0.8)";
-
-//     //按钮聚焦的处理
-//     if (n == 0) {
-//       btn_0.style.backgroundColor = bgFocus;
-//       btn_1.style.backgroundColor = bgNormal;
-//     } else if (n == 1) {
-//       btn_1.style.backgroundColor = bgFocus;
-//       btn_0.style.backgroundColor = bgNormal;
-//     }
-//   };
-
-//   let interval; //定时器编号
-//   let autoPlay = function () {
-//     //延迟3秒，切换下一张图片
-//     interval = setInterval(function () {
-//       switchimg("next");
-//     }, 3000);
-
-//     console.log("interval", interval);
-//   };
-
-//   //不用点击直接调用
-//   autoPlay();
-// };
 
 export default {
   data: function () {
@@ -84,6 +68,11 @@ export default {
 </script>
 
 <style scoped>
+.bigg {
+  background: #f8f8f8;
+  height: 1175px;
+  width: 100%;
+}
 .box_head {
   height: 90px;
   text-align: center;
@@ -92,93 +81,92 @@ export default {
   font-size: 40px;
   background-color: #fff;
   color: #000;
-  background-image: url(https://timgsa.baidu.com/timg?di=627560e…&image=&qu…%3A%2F%2Fimg.nongji360.com%2Fn%2Fimage%2F2015%2F08%2F11%2F151247223025.jpg);
-}
-/* .block {
-  width: 380px;
-  margin: 0 auto;
-  background: #f8f8f8;
-} */
-
-.my-swipe .van-swipe-item {
-  color: #fff;
-  font-size: 20px;
-  line-height: 150px;
-  text-align: center;
-  background-color: #39a9ed;
-}
-
-.box_banner1 {
-  overflow: hidden;
-  position: relative;
-  width: 100vw;
-  height: 180px;
-  margin-bottom: 20px;
-  justify-content: center;
-  display: flex;
-  margin: 0 auto 20px;
 }
 
 .box_img {
   width: 100vw;
-  height: 180px;
+  height: 135px;
 }
 
-.box_banner1 img {
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 135px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+
+.my-swipe img {
   width: 100%;
   height: 100%;
   /* 通过CSS3的object-fit调整图片的适应盒子规则 */
   object-fit: cover;
 }
 
-.box_banner1_in {
-  transition: 1s;
+.block {
   display: flex;
-  width: 300%;
-  left: 0;
+  width: 380px;
+  margin: 0 auto;
+  background: #f8f8f8;
+}
+.box_1 {
+  height: 265px;
+  width: 175px;
+  background: #fff;
+  margin: 5px 7px;
+  float: left;
+}
+.goods_img {
+  width: 167.5px;
+  height: 175.5px;
+  position: relative;
+}
+.box_commodity {
+  width: 167.5px;
+  height: 167.5px;
+  background-size: 167.5px 167.5px;
+  background-repeat: no-repeat;
+  margin: 0 4px;
+  position: absolute;
   top: 0;
-  position: absolute;
+  left: 0;
 }
-
-.box_prev_next {
+.img_logo {
+  width: 32px;
+  height: 32px;
   position: absolute;
-  display: flex;
-  padding: 0 10px;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  z-index: 6;
+  top: 0;
+  left: 10px;
 }
-
-.box_prev_next li {
-  cursor: pointer;
+.goods_title {
+  height: 40px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.box_2,
+.box_car {
+  color: #ff5e5c;
+  float: left;
+  margin: 20px 0;
+}
+.box_car {
+  font-size: 25px;
+  width: 40px;
+  height: 40px;
+}
+.plase {
+  height: 45px;
+}
+.lookAll {
+  display: block;
+  width: 380px;
+  height: 50px;
+  background-color: #fff;
+  clear: both;
   text-align: center;
-  color: #cccccc;
-  background-color: rgba(9, 9, 9, 0.3);
-  border-radius: 50%;
-  width: 25px;
-  height: 25px;
-}
-
-.box_banner_btn {
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 20px;
-  bottom: 10px;
-  z-index: 6;
-}
-
-.box_banner_btn li {
-  margin: 0 5px;
-  background-color: rgba(255, 255, 255, 0.8);
-  border-radius: 50%;
-  width: 15px;
-  height: 15px;
-  cursor: pointer;
-  text-align: center;
+  line-height: 50px;
+  text-decoration: underline;
+  margin-left: 0;
+  color: #333333;
 }
 </style>
